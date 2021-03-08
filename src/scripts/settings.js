87ca224cf011805bfea516a82b938ed3f7fd6dfc
Brayan Quirino <73717704+BrayanQuirinoSPS@users.noreply.github.com>
2021-03-08 16:28:20 -0600

    Update settings.js

--- src/scripts/settings.js
+++ src/scripts/settings.js
@@ -5,34 +5,9 @@ const isClientAuthEnabled = false;
 const lapi_1= '#ff7800'
 const lapi_2='#d94f18'
 const hover='#2e96af'
-const colorsOrange={    //custom colors property  
-    "branding": lapi_1,
-    "text": "#000",
-    //"textLight": "#737373",
-    //conversationBackground: "#fff",fafafa
-    //footerBackground: "#fff",
-    //headerButtonFill: "#fff",
-    //"timestamp": "#5b5652",
-    "userMessageBackground": "#fafafa",
-    "botMessageBackground": "#fafafa",
-    "actionsBackground": lapi_1,
-    "actionsBackgroundHover": hover,
-    "actionsBorder":"#fff",
-    "actionsText": "#fff",
-    "globalActionsBackground": lapi_1,
-    "globalActionsBackgroundHover": hover,
-    "globalActionsBorder": "#fff",
-    "globalActionsText": "#fff",
-    "globalActionsTextHover": "#fff",
-    //"links": "#0770bf",
-    "cardNavButton":lapi_1,
-    "cardNavText": "#fff",
-    "cardNavButtonHover": hover,
-    "recognitionViewButtonFill": "#fff",
-    "recognitionViewText": "#fff",
-    "typingIndicator": lapi_1
-}
-const colorsGray={    //custom colors property  
+
+//custom colors property  
+const colors_lapi_1={    //custom colors property  
     "branding": lapi_1,
     "headerBackground": lapi_1,
     "visualizer": "#fff",
@@ -67,7 +42,7 @@ const colorsGray={    //custom colors property
     "typingIndicator": lapi_1
 }
 
-const complete_colors={
+/*const complete_colors_settings={
     branding: "#025e7e",
 	text: "#161513",
 	textLight: "#3a3631",
@@ -101,64 +76,56 @@ const complete_colors={
 	recognitionViewText: "#fff",
 	notificationBadgeBackground: "#9a0007",
 	notificationBadgeText: "#fff"
-}
-/**
- * Initializes the SDK and sets a global field with passed name for which
- * it can be referred to later.
- *
- * @param {string} name Name by which the chat widget should be referred
- */
+}*/
+
 const initSdk = (name) => {
     if (!name) {
         name = 'Bots';          // Set default reference name to 'Bots'
     }
     let Bots;
-
+    //let log='src/images/logo1.png'
+    let logo='https://github.com/BrayanQuirinoSPS/notas/raw/master/Oracle_Digital_Assitant/Lapi/chatbot_web/src/images/logo1.png'
     setTimeout(() => {
-        /**
-            * SDK configuration settings
-            * Other than URI, all fields are optional with two exceptions for auth modes
-            * In client auth disabled mode, 'channelId' must be passed, 'userId' is optional
-            * In client auth enabled mode, 'clientAuthEnabled: true' must be passed
-            */
         let chatWidgetSettings = {
-            //URI: process.env.URI,
-            URI: 'oda-299d9648040a4a0abe6a946d4a14e717-da2.data.digitalassistant.oci.oraclecloud.com',
             //enableTimestamp: true,
             timestampFormat: "hh:mm a",
-            //enableBotAudioResponse: true,
-            //timestampMode: "relative",
-            //enableClearMessage: true,
-            //botButtonIcon: 'images/lapibot_1.png', 
-            logoIcon: 'https://github.com/BrayanQuirinoSPS/lapi_demo/raw/main/src/images/lapibot_2.png', 
-            botIcon: 'https://github.com/BrayanQuirinoSPS/lapi_demo/raw/main/src/images/lapibot_2.png',
-            //clearMessageIcon: 'src/images/eliminar.png',        
+            logoIcon: logo, 
+            botIcon: logo,
             //personIcon: 'images/user-icon.png', 
+            botButtonIcon: logo, 
+            //clearMessageIcon: 'src/images/eliminar.png',        
+            //enableClearMessage: true,
+            //timestampMode: "relative",
+            //enableBotAudioResponse: true,
             //letra y tema
             //font: '14px Verdana, Geneva, sans-serif',
             //theme: 'classic',
             //theme: 'redwood-dark'
+            // Método para embeber el bot en un div
             /*embedded: true,
             targetElement: 'chat-container',    
             embedTopStickyId: 'top-text',
             embedTopScrollId : 'top-text',
             customHeaderElementId: 'custom-header',*/
-            initUserHiddenMessage: 'Hola',
             //showConnectionStatus: true,
             //position: {bottom: '2px', right: '2px'},
+            //Inicializador del bot
+            initUserHiddenMessage: 'Hola',
             conversationBeginPosition: 'bottom',
             //Caracteristicas escondidas
             i18n: {
                 en: {
-                    chatTitle: 'LAPI Laboratorio Médico',       // Replaces Chat
-                    connected: 'Conectado',            // Replaces Connected
-                    inputPlaceholder: 'Escribe tu mensaje...', // Replaces Type a message
-                    send: 'Enviar (Enter)'           // Replaces Send tool tip
+                    chatTitle: 'LAPI Laboratorio Médico',       
+                    connected: 'Conectado',            
+                    inputPlaceholder: 'Escribe tu mensaje...', 
+                    send: 'Enviar (Enter)'  
                 }
             },
-            "colors": colorsGray,
+            "colors": colors_lapi_1,
             clientAuthEnabled: isClientAuthEnabled,
+            URI: 'oda-299d9648040a4a0abe6a946d4a14e717-da2.data.digitalassistant.oci.oraclecloud.com',
             channelId: 'abb40491-1b0d-4c90-916c-ed610498d558'
+            //URI: process.env.URI,
             //channelId: process.env.CHANNEL_ID 
         };
 
@@ -171,7 +138,7 @@ const initSdk = (name) => {
 
         // Connect to the ODA
         Bots.connect();
-
+        
         // Create global object to refer Bots
         window[name] = Bots;
     }, 0);
